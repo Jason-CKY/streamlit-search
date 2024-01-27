e32e13dbfea9aa8128bd9a261ff5111f860c7b6b
Jason Cheng <jasoncky96@gmail.com>
2024-01-28 00:04:33 +0800

    Update loading status with container loading and show assistant chat message

--- src/streamlit/core/search.py
+++ src/streamlit/core/search.py
@@ -1,32 +1,14 @@
 import asyncio
+import random
+from typing import List
 from loguru import logger
 from schemas.search import SearchResult
 from langchain_core.documents import Document
 from core.conversation_handler import ConversationHandler
 from core.settings import settings
 
-async def mock_search_results(search: str):
-    await asyncio.sleep(1)
-    handler = ConversationHandler(
-        model = settings.model_id,
-        temperature = 0,
-        max_tokens = 1024,
-    )
-    rag_chain = handler.get_rag_chain()
-    source_documents = [
-        Document(
-            page_content="test from source document", 
-            metadata={
-                "source_document": "https://google.com"
-            }
-        )
-    ]
-    response = await rag_chain.ainvoke({
-        "context": handler.format_docs(source_documents),
-        "question": search,
-    })
-    logger.info(response)
-
+async def mock_get_source_documents(search: str):
+    await asyncio.sleep(random.random() * 2)
     return [
         SearchResult(
             title=f"Article #{i+1} {search}",
@@ -34,3 +16,20 @@ async def mock_search_results(search: str):
             link="https://google.com",
         ) for i in range(20)
     ]
+
+async def get_rag_response(search_query: str, source_documents: List[Document]) -> str:
+    await asyncio.sleep(random.random() * 2)
+    handler = ConversationHandler(
+        model = settings.model_id,
+        temperature = 0,
+        max_tokens = 1024,
+    )
+    rag_chain = handler.get_rag_chain()
+    response = await rag_chain.ainvoke({
+        "context": handler.format_docs(source_documents),
+        "question": search_query,
+    })
+    logger.info(response)
+    return response
+
+
--- src/streamlit/streamlit_app.py
+++ src/streamlit/streamlit_app.py
@@ -1,9 +1,9 @@
 import asyncio
-import random
+import time
 import streamlit as st
 import templates
 from urllib import parse
-from core.search import mock_search_results
+from core.search import mock_get_source_documents, get_rag_response
 from core.settings import settings
 
 
@@ -17,7 +17,12 @@ def set_session_state():
         st.session_state._search = None
     if 'search_results' not in st.session_state:
         st.session_state._search = None
-        
+    if 'llm_response' not in st.session_state:
+        st.session_state.llm_response = None    
+    if 'query_time' not in st.session_state:
+        st.session_state.query_time = None
+
+
     # get parameters in url
     if 'search' in st.query_params:
         new_search = parse.unquote(st.query_params['search'])
@@ -38,17 +43,36 @@ async def main():
     search = st.text_input('Enter search words:', key="search", on_change=search_input_on_change)
     if search:
         if st.session_state._search != search:
-            with st.spinner('Wait for it...'):
-                results = await mock_search_results(search)
+            with st.status('Generating answers...', expanded=True) as status:
+                st.write('Searching for documents...')
+                
+                start_time = time.time()
+                results = await mock_get_source_documents(search)
+                query_time = time.time() - start_time
+                
+                st.write('Generating AI response...')
+                
+                start_time = time.time()
+                llm_response = await get_rag_response(search, results)
+                llm_response_time = time.time() - start_time
+
                 st.session_state._search = search
                 st.session_state.search_results = results
+                st.session_state.llm_response = llm_response
+                st.session_state.query_time = query_time
+                status.update(label="Generation complete", state="complete", expanded=False)
         else:
             results = st.session_state.search_results
+            llm_response = st.session_state.llm_response
+            query_time = st.session_state.query_time
 
         from_i = (st.session_state.page - 1) * settings.page_size
         paginated_results = results[from_i:from_i + settings.page_size]
+
+        # Show AI RAG Response
+        st.chat_message("assistant").write(llm_response)
         # show number of results and time taken
-        st.write(templates.number_of_results(len(results), random.random()),
+        st.write(templates.number_of_results(len(results), query_time),
                  unsafe_allow_html=True)
         # search results
         for i, result in enumerate(paginated_results):
